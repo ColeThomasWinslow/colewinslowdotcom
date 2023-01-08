0059186fc85b3ecba38162d7ac8a0bddb9a4084d
Cole Winslow <colethomaswinslow7@gmail.com>
2023-01-07 17:56:17 -0700

    fixed cards

--- src/Components/Pages/Landing/Projects.jsx
+++ src/Components/Pages/Landing/Projects.jsx
@@ -77,7 +77,7 @@ export default function Projects() {
   return (
     <div name="projects" className="portfolio">
       <h2 className="secondary-title">Programming Portfolio</h2>
-      <p className="portfolio-text">
+      <p className="hero-text">
         See what the power of coding I can do! Check out these <br />
         full-stack applications I've created below.
       </p>
